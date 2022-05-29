12e6a2c33d0a220dfc1d361a67ae5b465ac35f41
CIRCERO <56322082+CIRCERO@users.noreply.github.com>
2022-05-29 17:54:07 +0200

    update repo

---
+++ __init__.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/CIRCERO/repo.circero.git
+cd "#repo.circero"
+git init
+git add --all
+git commit -m "Initial Commit"
+git remote add origin https://github.com/CIRCERO/repo.circero.git
+git push -u origin master
+
---
+++ init.sh
@@ -0,0 +1,5 @@
+$git config --global user.name "CIRCERO"
+$git config --global user.email "kevyn@axxess.co.za"
+
+
+
